ce71f941c1023c88c3411d16ce782468e5d3b93d
La Fountain <github email address>
2018-12-28 11:06:22 -0500

    Import csv data and established relationships

--- db/migrate/20181228010228_create_helpwanteds.rb
+++ db/migrate/20181228010228_create_helpwanteds.rb
@@ -3,7 +3,7 @@ class CreateHelpwanteds < ActiveRecord::Migration[5.2]
     create_table :helpwanteds do |t|
       t.string :name
       t.string :location
-      t.decimal :wage_per_week
+      t.string :wage_per_week
       t.text :description
       t.boolean :housing_offered
       t.references :job, foreign_key: true
