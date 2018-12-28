2ac96f82386d6064d60107d58511da3b917e9c91
La Fountain <github email address>
2018-12-28 16:43:05 -0500

    Add funfacts api, need to complete all rspec test

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
       resources :jobs, only: [:index, :show, :create, :update, :destroy]
+      resources :funfacts, only: [:index, :show, :create, :update, :destroy]
     end
   end
 end
