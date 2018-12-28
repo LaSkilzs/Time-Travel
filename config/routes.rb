7e38ceb253ba48f882f6e94909b86ce8e429454f
La Fountain <github email address>
2018-12-28 17:24:17 -0500

    Complete api for generations minus rspec test

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace 'v1' do
       resources :jobs, only: [:index, :show, :create, :update, :destroy]
       resources :funfacts, only: [:index, :show, :create, :update, :destroy]
+      resources :generations, only: [:index, :show, :create, :update, :destroy]
     end
   end
 end
