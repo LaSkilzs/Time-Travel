83a6236ee1c1eb8c8c93abe434511efba62475f6
La Fountain <github email address>
2018-12-28 17:40:11 -0500

    Complete industry api minus rspec testing

---
+++ app/controllers/api/v1/industries_controller.rb
@@ -0,0 +1,50 @@
+module Api
+  module V1
+    class IndustriesController < ApplicationController
+
+      before_action :find_industry, only: [:show, :update, :destroy]
+
+      def index
+        @industries = Industry.all
+
+        render json: {status: 'SUCCESS', message: "Industries Loaded", data:@industries}, status: :ok
+      end
+
+      def show
+        render json: {status: 'SUCCESS', message: 'Industry Loaded', data:@industry}, status: :ok
+      end
+
+      def create
+        @industry = Industry.create(industry_params)
+
+        if @industry.valid?
+          render json: {status: 'SUCCESS', message: 'Industry Created', data:@industry}, status: :ok
+        else
+          render json: {status: 'ERROR', message: 'Industry NOT Created', data:@industry}, status: :unprocessable_entity
+        end
+      end
+
+      def update
+        if @industry.update(industry_params)
+          render json: {status: 'SUCCESS', message: 'Industry has been updated', data:@industry}, status: :ok
+        else
+          render json: {status: 'ERROR', messge: 'Industry HAS NOT Been Updated', data:@industry}, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        @industry.destroy
+        render json: {status: 'SUCCESS', message: 'Industry Destroyed', data:@industry}, status: :ok
+      end
+
+      private
+      def find_industry
+        @industry = Industry.find(params[:id])
+      end
+
+      def industry_params
+        params.permit(:name, :summary)
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
       resources :jobs, only: [:index, :show, :create, :update, :destroy]
       resources :funfacts, only: [:index, :show, :create, :update, :destroy]
       resources :generations, only: [:index, :show, :create, :update, :destroy]
+      resources :industries, only: [:index, :show, :create, :update, :destroy]
     end
   end
 end
